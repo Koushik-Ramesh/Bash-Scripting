62bd97ab7e8aac22e00bf717ce9ed1c30175b41a
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-08-20 23:35:13 +0530

    Added Echo command

---
+++ 01-SAMPLE.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# shebang Notation: This tells the script which intrepretor this script has to be executed
+echo Welcome to Bash-Scripting
+# This is how we can execute the scripts
+# $ bash scriptname.shor sh scriptname.sh or ./scriptname.sh
