3df61892e891e1cbe026551883f96e7e4c441926
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-08-26 15:46:44 +0530

    Autopush

--- 04-Variable.sh
+++ 04-Variable.sh
@@ -15,3 +15,8 @@ b=hii
 
 echo "Printing the value of a : $a"     # $a and ${a} both are same - Prefer using flower brackets
 echo "Print a : ${a}"
+
+Date="Aug 26th"
+echo "TODAY'S DATE IS $Date"
+# Data_Dir=Robot
+# rm -rf  /data/${Data_Dir}/    #rm -rf /data/      # rm -rf /data/robot/
