b01bc1c9f9f1589857208ede3769f7ad205595d5
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-08-27 00:12:12 +0530

    Autopush

---
+++ 10-Command.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# There are 4 types of commands
+# 1. Binary Commands (/bin = regular commands, /sbin= commands used by root user)
+# 2. Alias Commands (Shortcut commands)
+# 3. shell Commands (Builtin commands ex: cd, break )
+# 4. Functions (Set of commands that can be written in a sequence and called n number of times)
+
+#How to call a function ?
+
+#f()
+#{
+#    echo hi
+#}
