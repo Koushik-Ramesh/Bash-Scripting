13e09abecfcf2a584ad048a1916b459189b2ae63
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-08-26 23:24:17 +0530

    Autopush

--- 06-Special_Variables.sh
+++ 06-Special_Variables.sh
@@ -5,7 +5,8 @@
 
 # echo "Executed Scriptname is : 06-Special_Variables.sh"   # This is hardcoding
 echo "Executed scriptname is : $0"                          # $0 : Will give info of script name
-echo "Name of the city you are currently living in is $1"
+echo "The city you are currently living in is $1"
+echo "State which you are currently residing in $2"
 
 # bash scriptname.sh arg1 arg2 arg3
 # arg 1,2,3 will be the argument that will be comsidered during the script executing
