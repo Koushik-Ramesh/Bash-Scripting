efdfebf5b467f1aaffec27c4d1d15cf4aa6a55d2
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-08-26 23:58:44 +0530

    Autopush

--- 08-Quotes.sh
+++ 08-Quotes.sh
@@ -2,6 +2,6 @@
 
 # Single Quotes: Will print directly whatever is present between the single quotes
 # Double Quotes: Will print the values of the assigned variable in the double quotes
-
+echo "Capital of india is Delhi"
 echo "$*"
 echo '$*'
