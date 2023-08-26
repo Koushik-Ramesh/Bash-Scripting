64ea28b83dac68bf1dfea068da7bc846e5b5c2bb
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-08-26 15:18:09 +0530

    Autopush

---
+++ 04-Variable.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#What is variable ? It is generally used to hold some values dynamically
+
+# for ex: a=10, b=Hii - in this a&b are variables used to hold values 10 and xyz respectively
+a=10
+b=hii 
+# There is no concept of data types in linux or shellscripting
+# By default, everything is a string
+
+# If an input conains special characters, enclose with the double quotes
+
+# How to print the values of the variable ? - Using special charactes, we will be printing the values of the variable
+# echo $a
+
+echo "Printing the value of $a"     # $a and ${a} both are same - Prefer using flower brackets
+echo "Print ${a}
