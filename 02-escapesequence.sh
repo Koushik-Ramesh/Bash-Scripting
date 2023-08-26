ac5132fd6a18c4f11cef4abe6444c724b9721908
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-08-26 14:20:07 +0530

    Autopush

---
+++ 02-escapesequence.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# shebang Notation: This tells the script which intrepretor this script has to be executed
+# There can only be one intrepretor for one script
+echo Welcome to Bash-Scripting
+# This is how we can execute the scripts
+# $ bash scriptname.shor sh scriptname.sh or ./scriptname.sh
+
+# \n : This indicates new line
+# Koushik\nDevops
+# Koushik
+# Devops
+
+echo line 1
+echo line 2
+echo line 3
+echo line 4
+
+echo line1\nline2
