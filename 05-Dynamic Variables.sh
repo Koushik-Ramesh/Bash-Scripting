f91359438f21bb19582266aa562f28963d21baa0
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-08-26 23:02:42 +0530

    Autopush

--- 05-Dynamic Variables.sh
+++ 05-Dynamic Variables.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 DATE="$(date +%F)"
+# Who in xshell : List the sessions that are running in the system
+# wc -l : List the number of sessions
+Sessions_Count="$(who | wc -l)"
 echo "Today's Date is $DATE "
+echo "Number of sessions running: $Sessions_Count"
