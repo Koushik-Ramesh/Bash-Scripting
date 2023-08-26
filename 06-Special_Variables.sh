68c9001e48e40757064e03a3b7999eae5654006e
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-08-26 23:16:47 +0530

    Autopush

---
+++ 06-Special_Variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Special variables gives special properties to the variables
+# Ex: $0-$9, $?, $#, $*, $@
+
+# echo "Executed Scriptname is : 06-Special_Variables.sh"   # This is hardcoding
+echo "Executed scriptname is : $0"                          # $0 : Will give info of script name
