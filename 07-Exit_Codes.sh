14552609481321d240b9cf1d238bd78f14cefc23
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-08-29 00:07:08 +0530

    Autopush

--- 07-Exit_Codes.sh
+++ 07-Exit_Codes.sh
@@ -5,3 +5,9 @@
 # Code 0 represents the success of last command
 # code 1-255 represents either partial success, partial failure or complete failure
 
+# 0         : Global Success
+# 1 - 125   : Some failure in the commands
+# 125+      : System Failure
+
+
+# Exit codes also play key role in debugging big scripts
---
+++ 12-Conditions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Conditions helps us to execute something only if the factor satisfies the condition or else not
+
+# syntax
+# case $var in
+#    opt 1) Commands-x ;;
+#    opt 2) Commands-y ;;
+# esac
+
+Action=$1
+case $Action in
+    start)
+        echo "Starting the service"
+        ;;
+    stop)
+        echo "Stopping the service"
+        ;;
+    restart)
+        echo "Restarting the service"
+        ;;
+    *)
+        echo "Valid options are start, stop or restart
+        ;;
+esac
+
+
