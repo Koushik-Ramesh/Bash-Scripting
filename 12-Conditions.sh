b412d9a189d8cd9f8f3c2c8f4cc7b02be7507bba
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-08-29 00:21:38 +0530

    Autopush

--- 12-Conditions.sh
+++ 12-Conditions.sh
@@ -13,17 +13,25 @@ Action=$1
 case $Action in
     start)
         echo -e "\e[32m Starting the service \e[0m"
+        exit 0
         ;;
     stop)
         echo -e "\e[90m Stopping the service \e[0m"
+        exit 1
         ;;
     restart)
         echo -e "\e[36m Restarting the service \e[0m"
+        exit 2
         ;;
     *)
         echo -e "\e[31m Valid options are start, stop or restart \e[0m"
         echo -e "\e[31m Example: bash Scriptname restart \e[0m"
+        exit 3
         ;;
 esac
+
+# Exit 0,1,2... will let us know whether the command is executed properly or not
+# TO check exot code : $?
+
 
 
