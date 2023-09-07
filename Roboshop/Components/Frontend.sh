96a242a37b4319ac2402a2e4954537d0a6a72d17
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-09-07 22:26:35 +0530

    Autopush

--- Roboshop/Components/Frontend.sh
+++ Roboshop/Components/Frontend.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Validating the user who runs the script is root user or not
+# In linux , root user's gid or uid is always 0
+USER_ID=$(id -u)
+
+if [$USER_ID -ne 0]; then
+    echo -e "\e[32m Script must be executed as root user or sudo prefix \e[0m"
+     exit 1
+fi
+
 echo "Configuring Frontend"
 
 yum install nginx -y
