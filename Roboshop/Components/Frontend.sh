c44c01223ccdd4f1cd6092278eaa4b5a4b9f6dbc
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-09-07 22:36:01 +0530

    Autopush

--- Roboshop/Components/Frontend.sh
+++ Roboshop/Components/Frontend.sh
@@ -3,9 +3,9 @@
 # Validating the user who runs the script is root user or not
 # In linux , root user's gid or uid is always 0
 
-USER_ID=$(id -uid)
+USER_ID=$(id -u)
 
-if [$USER_ID -ne 0] ; then
+if [ $USER_ID -ne 0 ] ; then
     echo -e "\e[32m Script must be executed as root user or sudo prefix \e[0m"
     exit 1
 fi
