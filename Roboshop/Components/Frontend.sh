ed66ea0d782f2f1b98ffa00340880054d7ab2014
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-09-07 22:28:10 +0530

    Autopush

--- Roboshop/Components/Frontend.sh
+++ Roboshop/Components/Frontend.sh
@@ -4,7 +4,7 @@
 # In linux , root user's gid or uid is always 0
 USER_ID=$(id -u)
 
-if [$USER_ID -ne 0]; then
+if [$USER_ID -ne 0] ; then
     echo -e "\e[32m Script must be executed as root user or sudo prefix \e[0m"
      exit 1
 fi
