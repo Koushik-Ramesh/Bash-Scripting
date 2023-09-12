ba1160c3cf54a6921dc36c0d6842dd95174dc3e0
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-09-12 17:38:51 +0530

    Autopush

--- Roboshop/Components/mysql.sh
+++ Roboshop/Components/mysql.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-echo "I am mysql"
+USER_ID=$(id -u)
+Component=mysql
+Logfile="/tmp/${Component}.log"
+
+if [ $USER_ID -ne 0 ] ; then
+    echo -e "\e[32m Script must be executed as root user or sudo prefix \e[0m"
+    exit 1
+fi
+
+Status(){
+    if [ $1 -eq 0 ] ; then
+        echo -e "\e[32m Success \e[0m"
+    else 
+        echo -e "\e[31m Failure \e[0m"
+    fi
+
+}
+
+echo -e "Configuring the ${Component} repo: "
+curl -s -L -o /etc/yum.repos.d/${Component}.repo https://raw.githubusercontent.com/stans-robot-project/${Component}/main/${Component}.repo
+Status $?
+
+echo -e "Installing ${Component}: "
+yum install mysql-community-server -y       &>> ${Logfile}
+Status $?
+
