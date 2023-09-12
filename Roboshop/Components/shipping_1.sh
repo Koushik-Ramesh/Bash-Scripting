7e06e232338e247122a33695dddb85a48451a71e
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-09-13 00:37:10 +0530

    Autopush

---
+++ Roboshop/Components/shipping_1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Component=shipping
+
+source Components/common.sh         # Calling the script from another script to run it
+JAVA              # Calling the declared java function 
+
+echo -e "\e[35m Configuring ${Component} Complete.....! \e[0m"
