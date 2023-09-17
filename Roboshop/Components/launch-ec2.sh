bbe094db594a9f3cbb9a8e7e1e3cf56a1eb1b89d
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-09-18 01:17:37 +0530

    Autopush

--- Roboshop/Components/launch-ec2.sh
+++ Roboshop/Components/launch-ec2.sh
@@ -34,9 +34,9 @@ PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANC
 echo -e "Private IP address of the ${Component} is $PRIVATEIP \n"
 echo -e "Creating DNS Record of ${Component} :"
 
-sed -e "s/Component/${Component}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route53.json     > /tmp/r53.json
+sed -e "s/Component/${Component}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route53.json > /tmp/r53.json
 cat /tmp/r53.json
 
-aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch=file:///tmp/r53.json
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
 echo -e "Private IP address of the ${Component} is created and ready to use on ${Component}.robosop.internal"
 
