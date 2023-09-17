22079590c9bff40bd5504372bc863d5c0892b0d4
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-09-17 23:01:01 +0530

    Autopush

--- Roboshop/Components/launch-ec2.sh
+++ Roboshop/Components/launch-ec2.sh
@@ -10,10 +10,15 @@ To create a server, information needed are:
 COMMENT
 
 Component=$1
-AMI_ID="ami-0c1d144c8fdd8d690"
-INSTANCE_TYPE="t2.micro"-
-SG_ID="sg-0ca0742615a57b999"     #Security group id - koushik allow all
+if [ -z $1 ] ; then
+    echo -e "\e[31m Component name is required \e[0m \n\n\t"
+    echo -e "\e[36m Ex usage $ bash launch-ec2 \e[0m"
+    exit 1
+fi
 
+AMI_ID="ami-0c1d144c8fdd8d690"
+INSTANCE_TYPE="t2.micro"
+SG_ID="sg-0ca0742615a57b999"     #Security group id - koushik allow all
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=${Component}}]' ]
 
 # Each and every resource created will have tags
